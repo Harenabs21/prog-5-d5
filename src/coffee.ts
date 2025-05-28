2290b22b5f3ea90695dace5290da8c39b70d484b
Harenabs21 <hei.harena.6@gmail.com>
2025-05-28 09:35:52 +0300

    add restock and decrement stock method in coffee

--- src/coffee.ts
+++ src/coffee.ts
@@ -1,10 +1,12 @@
 export class Coffee {
   private name: string;
   private price: number;
+  private stock: number;
 
-  constructor(name: string, price: number) {
+  constructor(name: string, price: number, stock: number) {
     this.name = name;
     this.price = price;
+    this.stock = stock;
   }
 
   getName() {
@@ -12,5 +14,23 @@ export class Coffee {
   }
   getPrice() {
     return this.price;
+  }
+  getStock() {
+    return this.stock;
+  }
+
+  decrementStock(): void {
+    try {
+      if (this.stock <= 0) {
+        throw new Error(`The coffee ${this.name} is out of stock.`);
+      }
+      this.stock -= 1;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  restock(quantity: number): void {
+    this.stock += quantity;
   }
 }
